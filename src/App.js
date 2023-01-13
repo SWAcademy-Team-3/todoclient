3a4e37a135f71e751199dfd446f09e36598c3176
Im_younique <dyyim4725@gmail.com>
2023-01-13 14:27:54 +0900

    [Add] 기본 페이지 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { React } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./page/Home";
+import Receive from "./page/Receive";
+import My from "./page/My";
+import Friends from "./page/Friends";
 import Login from "./page/Login";
 import PageNotFound from "./page/PageNotFound";
 import Navigation from "./Navigation";
@@ -15,6 +18,9 @@ function App() {
         <div className="contents">
           <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/receive" element={<Receive />}></Route>
+            <Route path="/my" element={<My />}></Route>
+            <Route path="/friends" element={<Friends />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="*" element={<PageNotFound />}></Route>
           </Routes>

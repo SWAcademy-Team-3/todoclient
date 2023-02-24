479e5bb099b6d10cfaf9face8b5117b4c3c0c4b5
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2023-02-24 22:44:59 +0900

    [fix] Cookie 수동 저장으로 Login 구현

--- src/service/Cookie.js
+++ src/service/Cookie.js
@@ -3,8 +3,14 @@ import cookie from "react-cookies";
 // const expires = new Date();
 // expires.setMinutes(expires.getMinutes() + 60);
 
-export const setCookie = (name, value, options) => {
-  return cookie.save(name, value, { ...options });
+export const setCookie = (name, value, options, expireDate) => {
+  if (expireDate !== undefined) {
+    const expires = new Date();
+    expires.setDate(expires.getDate() + expireDate);
+    return cookie.save(name, value, { ...options, expires });
+  } else {
+    return cookie.save(name, value, { ...options });
+  }
 };
 
 export const getCookie = (name) => {

5f81b2706c45156e4afb70ff55a6e9f4d3b83c3d
Im_younique <48540492+DongYounYim@users.noreply.github.com>
2023-02-27 01:36:19 +0900

    [Add] 회원가입 양식 완성

--- src/page/Register.js
+++ src/page/Register.js
@@ -1,9 +1,50 @@
+import { axios_post } from "../api/api";
 import SignUpForm from "../components/SignUpForm";
+import { setCookie } from "../service/Cookie";
+import { useUser } from "../contexts/userProvider";
 import "../style/form.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
-  const onSubmit = () => {
+  const navigate = useNavigate();
+  const { changeUserData } = useUser();
+  const onSubmit = async (values) => {
     //TODO onSubmit 지정하기
+    const data = {
+      name: values.name,
+      memberId: values.memberId,
+      password: values.password,
+      passwordConfirm: values.passwordConfirm,
+      bio: values.bio,
+    };
+    const response = await axios_post("register", JSON.stringify(data), "json");
+    if (response === undefined) {
+      alert(response.errorText);
+    } else {
+      localStorage.setItem(
+        "userData",
+        JSON.stringify({
+          memberId: response.memberId,
+          newLetterCount: response.newLetterCount,
+          coinCount: response.coinCount,
+        })
+      );
+      setCookie(
+        "access_token",
+        response.access_token,
+        {
+          path: "/",
+          secure: true,
+        },
+        1
+      );
+    }
+    changeUserData({
+      memberId: response.memberId,
+      newLetterCount: response.newLetterCount,
+      coinCount: response.coinCount,
+    });
+    navigate("/");
   };
   return (
     <div className="FormContainer">

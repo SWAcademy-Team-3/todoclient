7395692f44fffdc3e0d975bf0f0ee543f6ccbf54
Im_younique <dyyim4725@gmail.com>
2023-03-03 14:27:38 +0900

    [chore] 회원가입 오류시 에러 메세지 출력

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { getCookie } from "../service/Cookie";
 
-const END_POINT = "http://49.50.163.197:8080";
+// const END_POINT = "http://49.50.163.197:8080";
+const END_POINT = "http://10.188.191.201:8088";
 const subUrl = {
   login: "/api/login",
   register: "/api/signup",
@@ -48,6 +49,7 @@ export const axios_post = async (url, sendData, type = "json") => {
     }
   } catch (e) {
     console.error("POST Error : ", e);
+    return e.response.data;
   }
 };
 
